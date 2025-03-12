f42b4928cb0a57a16b3e606b92c42cb28b1b81d7
Devin Halser <devin.hasler@students.ffhs.ch>
2025-03-12 17:37:23 +0100

    fixing deployment

--- app/components/ImageUpload.tsx
+++ app/components/ImageUpload.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, type ChangeEvent, type FormEvent } from "react";
+import React, { useState, type ChangeEvent } from "react";
 import { Trash2 } from "lucide-react";
 
 interface FileWithPreview extends File {
